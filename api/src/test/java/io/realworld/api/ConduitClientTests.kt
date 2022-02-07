8467b7e3eaeebf6709a49ef67ce822d989641319
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2022-02-08 01:15:04 +0530

    Reshuffled

--- api/src/test/java/io/realworld/api/ConduitClientTests.kt
+++ api/src/test/java/io/realworld/api/ConduitClientTests.kt
@@ -1,15 +1,38 @@
 package io.realworld.api
 
 import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import retrofit2.http.Query
 
 class ConduitClientTests {
 
     private val conduitClient = ConduitClient()
 
     @Test
-    fun `GET articles`(){
-        val articles = conduitClient.api.getArticles().execute()
-        assertNotNull(articles.body()?.articles)
+    fun `GET articles`(
+    ){
+        runBlocking {
+            val articles = conduitClient.api.getArticles()
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `GET articles by author`(
+    ){
+        runBlocking {
+            val articles = conduitClient.api.getArticles(author = "444")
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `GET articles by tags`(
+    ){
+        runBlocking {
+            val articles = conduitClient.api.getArticles(tag = "welcome")
+            assertNotNull(articles.body()?.articles)
+        }
     }
 }
